ea944ad5c1a3e9d94d40ebdeb7b5dd4ec7b2a08a
Bruno Emanuel <bemanuel.pe@gmail.com>
2018-09-02 00:00:35 -0300

    Upd

--- upd_versao.sh
+++ upd_versao.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo $1 > versao.txt
-cp versao.txt debian/.
-cp versao.txt centos/.
-cp versao.txt alpine/.
+echo $1 > version.txt
+cp version.txt debian/.
+cp version.txt centos/.
+cp version.txt alpine/.
+git commit -m 'versao nova' version.txt debian/version.txt centos/version.txt alpine/version.txt
+git tag -a $1 -m $(date +%Y%m%d)
+git push origin $1
